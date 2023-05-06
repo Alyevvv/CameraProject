bf3c262a47aeb97cae0056a8927eda9a26564e3c
Alyevvv <kironos185@gmail.com>
2023-05-06 11:11:28 +0700

    first commit

---
+++ sdk/build/intermediates/cxx/Debug/1d6s276n/logs/arm64-v8a/build_command_opencv_jni_shared.bat
@@ -0,0 +1,5 @@
+@echo off
+"C:\\Users\\Alwi\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  -C ^
+  "C:\\Users\\Alwi\\AndroidStudioProjects\\ProjectCamera\\sdk\\.cxx\\Debug\\1d6s276n\\arm64-v8a" ^
+  opencv_jni_shared
---
+++ sdk/build/intermediates/cxx/RelWithDebInfo/2b245v1b/logs/x86/configure_command.bat
@@ -0,0 +1,19 @@
+@echo off
+"C:\\Users\\Alwi\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\cmake.exe" ^
+  "-HC:\\Users\\Alwi\\AndroidStudioProjects\\ProjectCamera\\sdk\\libcxx_helper" ^
+  "-DCMAKE_SYSTEM_NAME=Android" ^
+  "-DCMAKE_EXPORT_COMPILE_COMMANDS=ON" ^
+  "-DCMAKE_SYSTEM_VERSION=21" ^
+  "-DANDROID_PLATFORM=android-21" ^
+  "-DANDROID_ABI=x86" ^
+  "-DCMAKE_ANDROID_ARCH_ABI=x86" ^
+  "-DANDROID_NDK=C:\\Users\\Alwi\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_ANDROID_NDK=C:\\Users\\Alwi\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620" ^
+  "-DCMAKE_TOOLCHAIN_FILE=C:\\Users\\Alwi\\AppData\\Local\\Android\\Sdk\\ndk\\23.1.7779620\\build\\cmake\\android.toolchain.cmake" ^
+  "-DCMAKE_MAKE_PROGRAM=C:\\Users\\Alwi\\AppData\\Local\\Android\\Sdk\\cmake\\3.22.1\\bin\\ninja.exe" ^
+  "-DCMAKE_LIBRARY_OUTPUT_DIRECTORY=C:\\Users\\Alwi\\AndroidStudioProjects\\ProjectCamera\\sdk\\build\\intermediates\\cxx\\RelWithDebInfo\\2b245v1b\\obj\\x86" ^
+  "-DCMAKE_RUNTIME_OUTPUT_DIRECTORY=C:\\Users\\Alwi\\AndroidStudioProjects\\ProjectCamera\\sdk\\build\\intermediates\\cxx\\RelWithDebInfo\\2b245v1b\\obj\\x86" ^
+  "-DCMAKE_BUILD_TYPE=RelWithDebInfo" ^
+  "-BC:\\Users\\Alwi\\AndroidStudioProjects\\ProjectCamera\\sdk\\.cxx\\RelWithDebInfo\\2b245v1b\\x86" ^
+  -GNinja ^
+  "-DANDROID_STL=c++_shared"
